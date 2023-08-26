d1acdf9cc208410613439553800a95d5579b4375
louis <vietnp14@gmail.com>
2023-08-26 21:55:06 +0700

    chore: Update deploy shell script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 if [ $CODEBUILD_BUILD_SUCCEEDING -eq 1 ]
 then
-  aws s3 sync --delete --cache-control max-age=2592000, public build/ s3://$S3
+  aws s3 sync build/ s3://$S3 --delete --cache-control max-age=2592000
 
   aws s3 cp s3://$S3/index.html s3://$S3/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html
 
